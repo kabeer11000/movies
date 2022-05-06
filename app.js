cbdbce05128d5e491b1408d3c49481143832fca0
kabeer11000 <kabeer@kabeersnetwork.tk>
2022-05-07 04:45:14 +0500

    Switched to webtorrent

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/WebTorrent');
 var app = express();
 app.set("json spaces", 3)
 app.use(logger('dev'));
---
+++ classes/WebTorrent.js
@@ -0,0 +1,8 @@
+const _webtorrent = require("webtorrent");
+class WebTorrent extends _webtorrent {
+    addPromised = (...args) => new Promise((resolve, reject) => {
+        this.add(...args, resolve)
+        // this.add(...args, (t) => t.on("ready", () => resolve(t)));
+    })
+}
+module.exports = WebTorrent;
